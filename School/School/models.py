d8109fc168fa86423d4990fdeff86143b1a1728c
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-02-11 06:22:41 -0500

    add user manager objecet

--- School/School/models.py
+++ School/School/models.py
@@ -7,13 +7,45 @@ from django.apps import apps as django_apps
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.postgres.fields import ArrayField
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save, pre_delete
+
+from django.contrib.auth.models import BaseUserManager
+
+class MyUserManager(BaseUserManager):
+    """
+    A custom user manager to deal with emails as unique identifiers for auth
+    instead of usernames. The default that's used is "UserManager"
+    """
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    username = models.CharField(null=False, unique=True, max_length=255)
     email = models.EmailField(null=False, unique=True, max_length=255)
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = MyUserManager()
 
 
 class Teacher(models.Model):
@@ -39,8 +71,7 @@ class Teacher(models.Model):
         max_length=3,
         choices=HONORFIC_TITLE,
         default=MR)
-    email_address = models.EmailField(blank=False, null=False, unique=True, max_length=255)
-    students = models.ManyToManyField(settings.STUDENT_MODEL, related_name="students", blank=True)
+    students = models.ManyToManyField(settings.STUDENT_MODEL, related_name="teachers", blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -78,7 +109,14 @@ class Student(models.Model):
     )
     grade_level = models.IntegerField(choices=GRADE_LEVEL, default=1)
     student_id = models.CharField(blank=False, null=False, max_length=255)
-    teachers = models.ManyToManyField(settings.TEACHER_MODEL, related_name='teachers', blank=True)
-
+    
     def __str__(self):
         return self.first_name +  " "  + self.last_name;
+
+
+@receiver(pre_save, sender=User)
+def UserName(sender, instance, **kwargs):
+    try:
+       instance.username = instance.email
+    except Exception as e:
+        print(str(e))
