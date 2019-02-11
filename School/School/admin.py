876a202c05907a5a9f5873289eda86197a908b82
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2019-02-11 06:21:39 -0500

    fix django admmin

--- School/School/admin.py
+++ School/School/admin.py
@@ -1,34 +1,41 @@
 from django.apps import apps as django_apps
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
+from django import forms
 User = get_user_model()
 Teacher = django_apps.get_model('School', 'Teacher')
 Student = django_apps.get_model('School', 'Student')
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display_links = ('username', 'email')
-    list_display = ('username', 'email', 'is_staff', 'is_superuser')
-    fields = ('username', 'email', 'is_staff', 'is_superuser')
+class StudentForm(forms.ModelForm):
+    hobby = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=Student.HOBBY_CHOICES)
+
+    class Meta:
+        model = Student
+        exclude = []
+
+
+class UserAdmin(UserAdmin):
+    list_display_links = ('email',)
+    list_display = ('email', 'is_staff', 'is_superuser',)
 
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display_links = ('first_name', 'last_name')
-    list_display = ('first_name', 'last_name', 'email_address')
-    fields = ('first_name', 'last_name', 'email_address', 'honorofic_title', 'students')
+    list_display = ('first_name', 'last_name',)
+    fields = ('first_name', 'last_name', 'honorofic_title', 'students', 'user',)
 
 
 class StudentAdmin(admin.ModelAdmin):
     list_display_links = ('first_name', 'last_name')
     list_display = ('first_name', 'last_name', 'grade_level', 'date_of_birth', 'student_id')
-    fields = ('first_name', 'last_name', 'grade_level', 'date_of_birth', 'student_id', 'teachers')
+    fields = ('first_name', 'last_name', 'grade_level', 'date_of_birth', 'student_id', 'teachers', 'hobby',)
+    form = StudentForm
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Teacher, TeacherAdmin)
-
-
-
 
